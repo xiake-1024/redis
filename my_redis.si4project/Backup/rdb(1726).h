c9de77df89a4383c5fca0401d57b49dfc5012b05
xiake-1024 <1306027770@qq.com>
2020-05-05 09:44:59 +0800

    rdb repretation

--- my_redis.si4project/Backup/rdb(1726).h
+++ my_redis.si4project/Backup/rdb(1726).h
@@ -41,36 +41,6 @@
  //当版本不在向后兼容时候，版本号加一
 #define RDB_VERSION 9 
 
-/*----------------------------# RDB is a binary format. There are no new lines or spaces in the file.
-52 45 44 49 53              # Magic String "REDIS"
-30 30 30 37                 # 4 digit ASCCII RDB Version Number. In this case, version = "0007" = 7
-----------------------------
-FE 00                       # FE = code that indicates database selector. db number = 00
-----------------------------# Key-Value pair starts
-FD $unsigned int            # FD indicates "expiry time in seconds". After that, expiry time is read as a 4 byte unsigned int
-$value-type                 # 1 byte flag indicating the type of value - set, map, sorted set etc.
-$string-encoded-key         # The key, encoded as a redis string
-$encoded-value              # The value. Encoding depends on $value-type
-----------------------------
-FC $unsigned long           # FC indicates "expiry time in ms". After that, expiry time is read as a 8 byte unsigned long
-$value-type                 # 1 byte flag indicating the type of value - set, map, sorted set etc.
-$string-encoded-key         # The key, encoded as a redis string
-$encoded-value              # The value. Encoding depends on $value-type
-----------------------------
-$value-type                 # This key value pair doesn't have an expiry. $value_type guaranteed != to FD, FC, FE and FF
-$string-encoded-key
-$encoded-value
-----------------------------
-FE $length-encoding         # Previous db ends, next db starts. Database number read using length encoding.
-----------------------------
-...                         # Key value pairs for this database, additonal database
-                            
-FF                          ## End of RDB file indicator
-8 byte checksum             ## CRC 64 checksum of the entire file.
-*/
-
-
-
 /* Defines related to the dump file format. To store 32 bits lengths for short
  * keys requires a lot of space, so we check the most significant 2 bits of
  * the first byte to interpreter the length:
